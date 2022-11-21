b7db1b094c4ff93a5639755ff6beb7d28dc7caaa
dimitricbrown <dimitricbrown@outlook.com>
2022-11-21 09:18:06 -0600

    Added final code

---
+++ main.js
@@ -0,0 +1,10 @@
+const quarterValue = (number) => {
+    const quarter = number / 4
+    return quarter
+}
+
+// Invoke the function and store the return value
+const returned = quarterValue(26)
+
+// Log the return value
+console.log(returned)
